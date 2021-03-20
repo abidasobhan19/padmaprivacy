78648bbcde3cb520ca2fd5b0f739e00e1ca4f0f8
abidasobhan19 <abidasobhan19@gmail.com>
2021-03-20 11:39:36 +0600

    20/03/21

---
+++ src/component/main.js
@@ -0,0 +1,169 @@
+import React from "react";
+// import "./style.css";
+
+const Main = () => {
+  return (
+    <div className="policy-container">
+      <h2>Padma Tracker Privacy Policy</h2>
+      <p>
+        Padma is one of the rising company for software and IOT based services
+        in Bangladesh offer by some young entrepreneurs. As part of our services
+        we built the Padma Tracker app as a Free app. This SERVICE is provided by
+        Padma at no cost and is intended for use as is.
+      </p>
+
+      <p>
+        This page is used to inform visitors regarding our policies with the
+        collection, use, and disclosure of Personal Information if anyone
+        decided to use our Service.
+      </p>
+
+      <p>
+        Padma Tracker is a GPS Tracking Service using GPS Tracking Devices installed in
+        vehicles or used by the customer for monitoring their concerned Physical
+        Assets. This App or application is used for Tracking the physical
+        devices only, and the data collected through the physical device. Only
+        the customers and or owners of these IOT devices and or assets will be
+        able to use this service for their Asset Tracking purposes. This is the
+        domain of the service of this Application.
+      </p>
+
+      <h4>How We Collect and Use Information</h4>
+      <p>
+        We collect the following types of information about you: Registration
+        and Profile Information. When you create an account and during the use
+        of the service, we may collect your personal information such as your
+        username, first and last name, email address, mobile phone number.After
+        you set up your account, we may ask for other profile and demographic
+        information, such as your organization name and details, like address,
+        phone number and city.
+      </p>
+
+      <p>
+        <b>Geolocation:</b> The Geolocation data will be collected from physical
+        IOT or physical devices bought by the customer of this Application. We
+        will collect Geolocation from the App only for showing the current
+        location of the user in the map to show distance from a specific vehicle
+        using Finder Devices.
+      </p>
+      <p>
+        <b>Messages:</b> Send Some Command from User Phone to Physical Devices.
+      </p>
+
+      <p>
+        We use this information to operate, maintain, and provide the features
+        and functionality of the Service and to communicate directly with the
+        customer, such as to send the customer alerts on the movements of the
+        (customer) user’s vehicles or assets which is only related to the
+        concerned customer’s vehicles or assets. The customer or user can
+        control the receipt of email and text alerts by visiting “Settings”
+        within the mobile app or email. We may also send the Service-related
+        emails (e.g., account verification, order confirmations, change or
+        updates to features of the Service, technical and security notices)
+        related to the Service the customer has opted for.
+      </p>
+
+      <h4>Sharing of Your Information</h4>
+      <p>
+        We share your personal information, including name, email, and location
+        with others only within the organization or group used in Shokun
+        services.
+      </p>
+      <p>
+        We may at any time buy or sell/divest the company, or any combination of
+        its products, services, assets and/or businesses. Personal information
+        such as customer names and email addresses, and other user information
+        related to the Service will likely be one of the items transferred in
+        these types of transactions. We may also share, transfer or assign such
+        information in the course of corporate divestitures, mergers,
+        acquisitions, bankruptcies, dissolutions or similar transactions or
+        proceedings. Except as described above, we will not disclose personal
+        information to third parties (including law enforcement, other
+        government entity, or civil litigant; excluding our subcontractors)
+        unless required to do so by law or subpoena or if in our sole
+        discretion, we determine it is necessary to (a) conform to the law,
+        comply with legal process, or investigate, prevent, or take action
+        regarding suspected or actual illegal activities; (b) to enforce our
+        Terms of Use, take precautions against liability, to investigate and
+        defend ourselves against any claims or allegations, or to protect the
+        security or integrity of our site; and/or (c) to exercise or protect the
+        rights, property, or personal safety of Padma, our Users or others.
+      </p>
+
+      <h4>How We Protect Your Information</h4>
+
+      <p>
+        padma cares about the security of your information and uses
+        commercially reasonable physical, administrative, and technological
+        safeguards to preserve the integrity and security of all information we
+        collect and that we share with our service providers. However, no
+        security system is impenetrable and we cannot guarantee the security of
+        our systems 100%. In the event that any information under our control is
+        compromised as a result of a breach of security, we will take reasonable
+        steps to investigate the situation and where appropriate, notify those
+        individuals whose information may have been compromised and take other
+        steps, in accordance with any applicable laws and regulations. Padma
+        maintains proper backup of your data, but not everything or related data
+        can be guaranteed, so if upon any unforeseeable reason the data backups
+        fail then Padma will notify the user but cannot do anything more than
+        that.
+      </p>
+
+      <h4>Security</h4>
+      <p>
+        we value your trust in providing us your Personal Information, thus we
+        are striving to use commercially acceptable means of protecting it. But
+        remember that no method of transmission over the internet, or method of
+        electronic storage is 100% secure and reliable, and we cannot guarantee
+        its absolute security.
+      </p>
+
+      <h4>Links to Other Sites</h4>
+      <p>
+        This Service may contain links to other sites. If you click on a
+        third-party link, you will be directed to that site. Note that these
+        external sites are not operated by us. Therefore, we strongly advise you
+        to review the Privacy Policy of these websites. we have no control over
+        and assume no responsibility for the content, privacy policies, or
+        practices of any third-party sites or services.
+      </p>
+
+      <h4>Children’s Privacy</h4>
+      <p>
+        These Services do not address anyone under the age of 13. we do not
+        knowingly collect personally identifiable information from children
+        under 13. In the case we discover that a child under 13 has provided us
+        with personal information, we immediately delete this from our servers.
+        If you are a parent or guardian and you are aware that your child has
+        provided us with personal information, please contact us so that we will
+        be able to do necessary actions.
+      </p>
+
+      <h4>Changes to This Privacy Policy</h4>
+      <p>
+        we may update our Privacy Policy from time to time. Thus, you are
+        advised to review this page periodically for any changes. we will notify
+        you of any changes by posting the new Privacy Policy on this page. These
+        changes are effective immediately after they are posted on this page.
+      </p>
+
+      <h4>Your Privacy Rights as in Bangladesh</h4>
+      <p>
+        Padma collects various types of personal information about you both
+        online and offline, as described in this privacy policy. Under the law
+        of Bangladesh, Padma customers who are residents of Bangladesh or we
+        the service of Padma, may have to disclose their data or information
+        till the last available or retained data to third parties including
+        government entities according to the law of the land as required. We
+        will comply with such request which will be applicable based on the law
+        of the country.
+      </p>
+      <p>
+        You may send us an email at info@forbitbd.com with the words “Privacy
+        Policy in Bangladesh” in the subject line.
+      </p>
+    </div>
+  );
+};
+
+export default Main;
